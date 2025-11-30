ac80b63b243e44e457132b2833a64af34ddf79bf
Shuhaib Mehri <mehri2@convai-srv-04.cs.illinois.edu>
2025-11-30 11:00:18 -0600

    updating bugs

--- full_human_study.py
+++ full_human_study.py
@@ -278,7 +278,7 @@ def show_study_intro_page():
         
         for i, pref in enumerate(config["preferences"], 1):
             st.markdown(f"**{i}.** {pref}")
-        st.markdown(f"Feel free to apply any additional preferences that help you solve the problem.")
+        st.markdown(f"Feel free to apply any additional preferences of your own that help you with solving the problem.")
     
     st.write("")
     st.markdown("**Remember these preferences as you interact with the agent throughout the sessions.**")
